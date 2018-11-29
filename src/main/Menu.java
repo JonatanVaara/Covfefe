f5285b05c4e5b78f4d07a73b25eb0e4b6bd055b7
JonatanVaara <44057466+JonatanVaara@users.noreply.github.com>
2018-11-29 11:27:53 +0100

    Created main and ProjectManagement-classes

---
+++ src/main/Menu.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Menu {
+	
+	public static void main(String[] args) {
+		final String type = "Jonatan";
+		if (type.equals("Jonatan")) {
+			// DETTA ÄR JONATANS MAIN SERU
+			System.out.println("Hej");
+		} else if(type.equals("Christian")) {
+			//DETTA ÄR CHRISTIANS DEL
+			System.out.println("Jag heter christian");
+		}
+	}
+}
---
+++ src/projectManagement/ProjectManagement.java
@@ -0,0 +1,5 @@
+package projectManagement;
+
+public class ProjectManagement {
+
+}
