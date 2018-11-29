2e518ecdc85bbf5cfd05e1472543d5d62e9a3ef4
JonatanVaara <44057466+JonatanVaara@users.noreply.github.com>
2018-11-29 11:34:49 +0100

    Some ProjectManagement basic stuff

--- src/projectManagement/ProjectManagement.java
+++ src/projectManagement/ProjectManagement.java
@@ -1,5 +1,27 @@
 package projectManagement;
 
-public class ProjectManagement {
+import java.util.ArrayList;
+import risk.RiskMatrix;
 
+public class ProjectManagement {
+	
+	private ArrayList<Member> memberList; 
+	private Schedule plannedSchedule;
+	private Schedule currentSchedule;
+	private RiskMatrix riskMatrix;
+
+	public ProjectManagement() {
+		
+		this.memberList = new ArrayList<Member>();
+		this.plannedSchedule = new Schedule();
+		this.currentSchedule = new Schedule();
+		this.riskMatrix = new RiskMatrix();
+		
+	}
+	
+	public double getEarnedValue() {
+	return 5.5;	
+	}
+	
+	
 }
