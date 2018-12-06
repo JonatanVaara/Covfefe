0d82c528bc193bbd291979028b1a0c7cd0b2969d
JonatanVaara <44057466+JonatanVaara@users.noreply.github.com>
2018-12-06 10:01:11 +0100

    ProjectManagementUpdate

--- src/projectManagement/ProjectManagement.java
+++ src/projectManagement/ProjectManagement.java
@@ -1,27 +1,63 @@
 package projectManagement;
 
 import java.util.ArrayList;
+
+import member.Member;
 import risk.RiskMatrix;
 
 public class ProjectManagement {
-	
-	private ArrayList<Member> memberList; 
+
+	private ArrayList<Member> memberList;
 	private Schedule plannedSchedule;
 	private Schedule currentSchedule;
 	private RiskMatrix riskMatrix;
 
 	public ProjectManagement() {
-		
+
 		this.memberList = new ArrayList<Member>();
 		this.plannedSchedule = new Schedule();
 		this.currentSchedule = new Schedule();
 		this.riskMatrix = new RiskMatrix();
+
+	}
+
+	public double getEarnedValue() {
+		double earnedValue = getBudgetAtCompletion() * 1;//percentageOfCompletion
+		return earnedValue;
+	}
+
+	public double getScheduledVariance() {
+		double scheduledVariance = getEarnedValue() - 1;//plannedValue
 		
+		return scheduledVariance;
+				
 	}
 	
-	public double getEarnedValue() {
-	return 5.5;	
+	public double getCostVariance() {
+		double costVariance = getEarnedValue() - 1;//getActualValue();
+		return costVariance;
 	}
+	
+	public double getTotalTime() {
+		double totalTime = 0;
+		for(Member member : memberList) {
+			totalTime += member.getAllocatedTaskTime();
+		}
+		return totalTime;
+	}
+	
+	public double getBudgetAtCompletion() {
+		double budgetAtCompletion = 0;
+		for (Member member : memberList) {
+			budgetAtCompletion += member.getPlannedTaskTime()*member.getSalary();
+		}
+		return budgetAtCompletion;
+	}
+	
+	public double getActualCost() {
+		return 555.5;
+	}
+	
 	
 	
 }
