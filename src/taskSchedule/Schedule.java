1c37c26ca14eae01b8ce9a77c17dc89c20a99a4c
strand93 <43995724+strand93@users.noreply.github.com>
2018-12-06 10:03:19 +0100

    Updated Task and Schedule

--- src/taskSchedule/Schedule.java
+++ src/taskSchedule/Schedule.java
@@ -10,7 +10,22 @@ public class Schedule {
 		
 		this.tasks = tasks;
 	}
-
+	
+	public Task searchTaskByName(String name) {   //Search for task by name and get that task
+		
+		for (Task task : this.getTasks()) {
+			
+			if(task.getName().equals(name)){
+				return task;
+			}
+		}
+		return null;
+	}
+	
+	//-------------------------
+	//--GETTERS AND SETTERS----
+	//-------------------------
+	
 	public ArrayList<Task> getTasks() {
 		return tasks;
 	}
