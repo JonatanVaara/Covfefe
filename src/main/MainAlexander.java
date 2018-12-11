063ec7cfe71ad0644307728f1b6d30f47f5d4a8d
strand93 <43995724+strand93@users.noreply.github.com>
2018-12-11 11:35:55 +0100

    Update MainAlexander.java

--- src/main/MainAlexander.java
+++ src/main/MainAlexander.java
@@ -9,13 +9,10 @@ public class MainAlexander {
 	public static void main(String[] args) {
 		
 		JsonReader reader = new JsonReader();
-		
-		Schedule plannedSchedule = new Schedule(reader.plannedScheduleReader());
-		Schedule currentSchedule = new Schedule(reader.currentScheduleReader());
 
 		//System.out.println(plannedSchedule.getTasks());
 		
-		ProjectSchedule ps = new ProjectSchedule(plannedSchedule, currentSchedule);
+		ProjectSchedule ps = new ProjectSchedule(reader.plannedScheduleReader(), reader.currentScheduleReader());
 		ps.plotChart();
 	}
 
